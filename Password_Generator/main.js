a057b07932834a564422724ac65b345c0fb0c72e
joolyen <julianivey@gmail.com>
2020-07-18 17:41:11 -0500

    trying push to combine arrays

--- Password_Generator/main.js
+++ Password_Generator/main.js
@@ -9,21 +9,34 @@ console.log(nums, specialChars)
 
 function generatePassword() {
 
-    let userLowercase = confirm("Would you like to use lowercase letters in your password?");
-    let userUppercase = confirm("Would you like to use uppercase letters in your password?");
-    let userNums = confirm("Would you like to use numbers in your password?");
-    let userSpecialChars = confirm("Would you like to use special characters in your password?");
-    
+    let userLowercase = confirm("Would you like to use lowercase letters in your password?");   
     if (userLowercase){
-        finalResults.push(lowerCase);
+        finalResults.concat(lowerCase);
     }
-    elseif (userLowercase && userUppercase){
+    else {
+
+    }
+    let userUppercase = confirm("Would you like to use uppercase letters in your password?");
+    if (userLowercase){
         finalResults.push(upperCase);
     }
     else {
-        finalResults.push(nums) 
-    }
 
+    }
+    let userNums = confirm("Would you like to use numbers in your password?");
+    if (userLowercase){
+        finalResults.push(nums);
+    }
+    else {
+
+    }
+    let userSpecialChars = confirm("Would you like to use special characters in your password?");
+    if (userLowercase){
+        finalResults.push(specialChars);
+    }
+    else {
+
+    }
     console.log(finalResults);
 
     // randomize finalResults
