731ccc4c4402a95f55f513ee2aa5d437a6bce2e6
joolyen <julianivey@gmail.com>
2020-07-19 15:53:21 -0500

    added an object with all of the functions I created to randomly generate the characters needed. I also added the DOM elements too.

--- Password_Generator/main.js
+++ Password_Generator/main.js
@@ -1,6 +1,21 @@
 
 // I worked with my tutor early on and what I was doing didn't make since to me so I found a this youtube video and this makes since for the generator functions - https://www.youtube.com/watch?v=duNmhKgtcsI
 
+// DOM Elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
+
 // Generator functions - http://www.net-comber.com/charset.html
 
 function getRandomLower(){
