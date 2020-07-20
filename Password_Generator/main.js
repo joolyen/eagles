67ef6634de29e551c1e79b07eab6b4ddffeddac3
joolyen <julianivey@gmail.com>
2020-07-20 18:20:48 -0500

    I've made a lot of updates to the js file because I noticed the homework files were available

--- Password_Generator/main.js
+++ Password_Generator/main.js
@@ -3,23 +3,41 @@
 
 // DOM Elements
 const resultEl = document.getElementById('result');
-const lengthEl = document.getElementById('length');
 const generateEl = document.getElementById('generate');
-const clipboardEl = document.getElementById('clipboard');
+
+// Object
+
+var password = {
+    lower: false,
+    upper: false,
+    number: false,
+    symbol: false,
+    length: 0
+}
+
+if(generateEl){
+     generateEl.addEventListener('click', (e) => {
+         e.preventDefault();
+         passwordCriteriaHandler();
+         console.log(password);
+     });
+ }
 
 
-
-const randomFunc = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    number: getRandomNumber,
-    symbol: getRandomSymbol
-};
 
 // Generator functions - http://www.net-comber.com/charset.html
 
+function getPasswordLength(){
+     password.length = prompt("Please choose a password between 8 and 128 characters long.");
+     if(length < 8 || length > 128){
+        alert("that's not the right size");
+        password.length = prompt("Please choose a password between 8 and 128 characters long.");
+     }
+}
+
 function getRandomLower(){
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    
 }
 function getRandomUpper(){
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
@@ -31,3 +49,43 @@ function getRandomSymbol(){
     const symbols = '!@#$%^&*(){}[]=<>/,.';
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
+
+function passwordCriteriaHandler(){
+    getPasswordLength();
+    confirmLowercase();
+    confirmUppercase();
+    confirmNumber();
+    confirmSymbol();
+}
+
+function confirmLowercase(){
+    password.lower = confirm("Would you like to include lowercase characters in your password?");
+}
+
+function confirmUppercase(){
+    password.upper = confirm("Would you like to include uppercase characters in your password?");
+}
+
+function confirmNumber(){
+    password.number = confirm("Would you like to include numbers in your password?");
+}
+
+function confirmSymbol(){
+    password.symbol = confirm("Would you like to include special characters in your password?");
+}
+
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
